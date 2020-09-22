bf8626dd0f086f07e796b67b41933ac7394fa302
leon2401119 <leon2401119.cs04@nctu.edu.tw>
2020-09-22 21:58:56 +0800

    ftp file transfer automated

--- pguy.py
+++ pguy.py
@@ -2,13 +2,95 @@ import os
 import glob
 import sys
 import argparse
+from ftplib import FTP
+import getpass
+import zipfile
 
 
 IODIR = 'stdio'
 
 
-def ftp_getfile(id):
-	pass
+def ftp_getfile(id, init):
+
+	if init:
+		try:
+			os.mkdir('.ftpinfo')
+		except Exception as e:
+			pass
+
+		server, port, hw_postfix, username, pwd = set_config()
+
+
+	else:
+		try:
+			f = open(os.path.join('.', '.ftpinfo', 'info'), 'r')
+			f.close()
+			server, port, hw_postfix, username, pwd = get_config()
+
+		except Exception as e:
+			os.mkdir('.ftpinfo')
+			server, port, hw_postfix, username, pwd = set_config()
+
+
+	ftp = FTP()
+	ftp.connect(server, int(port))
+	ftp.login(username, pwd)
+	ftp.cwd('hw')
+	ftp.cwd('hw' + hw_postfix)
+
+	ftp_file_list = ftp.nlst()
+	id_file_list = []
+	version = 0
+	src_file = None
+	for file in ftp_file_list:
+		if id in file:
+			v = int(file[-5:-4])
+			if v > version:
+				src_file = file
+
+
+	if not src_file:
+		print(f'zip file for {id} not found on ftp')
+		return False
+
+	else:
+		# file found for student
+		with open(src_file, 'wb') as f:
+			ftp.retrbinary(f'RETR {src_file}', f.write)
+
+		with zipfile.ZipFile(src_file, 'r') as zip_ref:
+			zip_ref.extractall('.')
+
+		os.remove(src_file)
+		return True
+
+
+
+
+
+
+def set_config():
+	with open(os.path.join('.', '.ftpinfo', 'info'), 'w') as f:
+		print('server url: ', end='')
+		server = input()
+		print('server port: ', end='')
+		port = input()
+		print('hw number: ', end='')
+		hw_postfix = input()
+		print('username: ', end='')
+		username = input()
+		pwd = getpass.getpass(prompt='password: ')
+
+		f.writelines([server + '\n', port + '\n', hw_postfix + '\n', username + '\n', pwd + '\n'])
+
+		return server, port, hw_postfix, username, pwd
+
+
+def get_config():
+	with open(os.path.join('.', '.ftpinfo', 'info'), 'r') as f:
+		server, port, hw_postfix, username, pwd = f.read().splitlines()
+
+	return server, port, hw_postfix, username, pwd
 
 
 def syspause_cleaner(filename):
@@ -40,9 +122,6 @@ def syspause_cleaner(filename):
 
 
 def main(id):
-	clean_up_list = glob.glob(os.path.join('.', '*.txt'))
-	for file in clean_up_list:
-		os.remove(file)
 
 	test_list = glob.glob(os.path.join('.', IODIR, '*.in'))  # 1-2.in
 
@@ -65,7 +144,7 @@ def main(id):
 
 		## compile phase
 
-		os.system(f'g++ {id}_{problem_num}.cpp -o {problem_num} 2>{problem_num}_err.txt 1>&2')
+		os.system(f'g++ {id}_{problem_num}.cpp -o {problem_num} 2>{problem_num}_err.txt')
 		if os.stat(os.path.join('.',f'{problem_num}_err.txt')).st_size:
 			# compile error
 			print(f'Score for problem {problem_num} : 0 (Compile Error)')
@@ -83,7 +162,7 @@ def main(id):
 
 			os.system(f'{prog} <{prefix}-{test_num}.in >{problem_num}-{test_num}.txt 2>&1')
 			# if os.name == 'posix':
-			# 	os.system(f'diff -B --strip-trailing-cr {prefix}-{test_num}.out {problem_num}-{test_num}.txt >{problem_num}-{test_num}_diff.txt')
+			# 	os.system(f'diff -B {prefix}-{test_num}.out {problem_num}-{test_num}.txt >{problem_num}-{test_num}_diff.txt')
 			# else:
 			# 	diff(f'{prefix}-{test_num}.out',f'{problem_num}-{test_num}.txt', f'{problem_num}-{test_num}_diff.txt')
 			diff(f'{prefix}-{test_num}.out', f'{problem_num}-{test_num}.txt', f'{problem_num}-{test_num}_diff.txt')
@@ -122,10 +201,19 @@ def diff(file1,file2,outfile):
 	out.close()
 
 
+def clean_dir():
+	clean_up_list = glob.glob(os.path.join('.', '*.txt'))
+	clean_up_list.extend(glob.glob(os.path.join('.', '*.cpp')))
+	for file in clean_up_list:
+		os.remove(file)
+
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser(description="i'm pguy. i p guy")
 	parser.add_argument('id', type=str, help='student id')
+	parser.add_argument('--init', '-i', action='store_true' , help='init/interactive mode for ftp server configuration')
 	args = parser.parse_args()
-	ftp_getfile(args.id)
-	main(args.id)
+	clean_dir()
+	res = ftp_getfile(args.id, args.init)
+	if res:
+		main(args.id)
